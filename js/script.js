bc01b18c2c0afd261983eb6e7b54d8bac5bcff9f
GiovanniCalia <johnca@outlook.it>
2022-02-21 16:15:26 +0100

    Tentativo esercizio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-let userKilometerTraveled = prompt ("quanti kilometri vuoi percorrere");
-let userAge = prompt ("Quanti hanni hai?");
+let userKilometerTraveled = parseInt(prompt ("quanti kilometri vuoi percorrere"));
+if (isNaN(userKilometerTraveled)){
+    alert(`We need a number`)
+}
+
+let PrezzoAlKm = 0.21;
+
+let userAge = parseInt(prompt ("Quanti hanni hai?"));
+if (isNaN(userAge)){
+    alert(`We need a number`)
+}
+
+/*let userTicketCost = parseInt(prompt ("prezzo biglietto"));
+if (isNaN(userTicketCost)){
+    alert(`We need a number`)
+}*/
+
 let element = document.getElementById("content");
 
-element.innerHTML = `Età passeggero: ${userAge} anni. Chilometri da percorrere: ${userKilometerTraveled} km`;
+/*element.innerHTML = `Età passeggero: ${userAge} anni. Chilometri da percorrere: ${userKilometerTraveled} km.`;*/
+
+
+if (userAge > 18){
+    let prezzofinale = ( userKilometerTraveled * PrezzoAlKm) - 20 * 100;
+    document.getElementById("content").innerHTML = `Il prezzo del biglietto è + ${prezzofinale;}`;
+}
+
+
+
