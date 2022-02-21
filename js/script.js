2f6f1b2ee43d58e10b2339968c609fb20c3534e2
GiovanniCalia <johnca@outlook.it>
2022-02-21 18:04:58 +0100

    Completamento dell'esercizio, rivisitato

--- js/script.js
+++ js/script.js
@@ -16,13 +16,13 @@ let element2 = document.getElementById("text_output2");
 element.innerHTML = `Età passeggero: ${userAge} anni. Chilometri da percorrere: ${userKilometerTraveled} km.`;
 
 if (userAge < 18){
-    let prezzofinale = (userKilometerTraveled * PrezzoAlKm) - (userKilometerTraveled * PrezzoAlKm * 0.2);
+    let prezzofinale = Math.round((userKilometerTraveled * PrezzoAlKm) - (userKilometerTraveled * PrezzoAlKm * 0.2));
     element2.innerHTML = `Il prezzo del biglietto è ${prezzofinale}€`;
 } else if (userAge > 65){
-    let prezzofinale = (userKilometerTraveled * PrezzoAlKm) - (userKilometerTraveled * PrezzoAlKm * 0.4);
+    let prezzofinale = Math.round((userKilometerTraveled * PrezzoAlKm) - (userKilometerTraveled * PrezzoAlKm * 0.4));
     element2.innerHTML = `Il prezzo del biglietto è ${prezzofinale}€`;
 } else{
-    let prezzofinale = (userKilometerTraveled * PrezzoAlKm);
+    let prezzofinale = Math.round((userKilometerTraveled * PrezzoAlKm));
     element2.innerHTML = `Il prezzo del biglietto è ${prezzofinale}€`;
 }
 
